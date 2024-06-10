6885751872c0025482b5fbfef084b55b89b7cbe5
J3ns3its <168995922+J3ns3its@users.noreply.github.com>
2024-06-10 10:45:47 +0200

    Player animations (#5)
    * added playermodel with collider, jump function and gravity
    
    * added testing model for guard. implemented simple animation while moving, fixed pants misalligned masking map of soldier, added colliders to selected street tiles
    
    * Remove .vscode
    
    ---------
    
    Co-authored-by: Jens Hartmann <hartmannje@cip.ifi.lmu.de>
    Co-authored-by: matias-gonz <maigonzalez@fi.uba.ar>

--- Assets/Scripts/EnemyControlerScript.cs
+++ Assets/Scripts/EnemyControlerScript.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 
 public class EnemyControllerScript : MonoBehaviour
 {
-    public float speed = 5f;
+    Animator animator;
+    public float walkingSpeed = 1.5f;
+    public float runningSpeed = 6f; //must be 4 times higher than speed
+    public float animationMaxSpeed = 2f;
+    float animationSpeed = 0f;
+    float currentSpeed = 4f; //formerly speed
+    
 
     private Rigidbody _rigidbody;
     private float _angularVelocity;
     private float _turnSmoothTime = 0.1f;
     private Vector3 _direction;
+    int VelocityXHash; //sidewards animation, can be use for strafe walking
+    int VelocityZHash; //forwards animation
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();  
+        VelocityXHash = Animator.StringToHash("Velocity X");
+        VelocityZHash = Animator.StringToHash("Velocity Z");
+        currentSpeed = walkingSpeed; //for testing purposes
+     
     }
 
     private void FixedUpdate()
@@ -27,8 +40,11 @@ public class EnemyControllerScript : MonoBehaviour
         float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
         float angle = Mathf.SmoothDampAngle(this.transform.eulerAngles.y, targetAngle, ref _angularVelocity,
             _turnSmoothTime);
-        _rigidbody.MovePosition(transform.position + _direction * (speed * Time.fixedDeltaTime));
+        _rigidbody.MovePosition(transform.position + _direction * (currentSpeed * Time.fixedDeltaTime));
         transform.rotation = Quaternion.Euler(0, angle, 0);
+        // maps current Speed to animation speed boundaries
+        animationSpeed = (currentSpeed/runningSpeed) * animationMaxSpeed; 
+        animator.SetFloat(VelocityZHash, animationSpeed);
     }
 
     public void MoveTo(Vector3 position)
---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    Animator animator;
+    int VelocityXHash;
+    int VelocityZHash;
+    float forwardMovement;
+    float sidewardMovement;
+    public CharacterController controller;
+    public Transform cam;
+    //float turnSpeed = 90f; // degrees per second
+    public float speedMultiplier = 3f;
+    public float jumpSpeed = 4f;
+    public float gravity = 9.8f;
+    private float vSpeed = 0f; // current vertical velocity
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        VelocityXHash = Animator.StringToHash("Velocity X");
+        VelocityZHash = Animator.StringToHash("Velocity Z");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardMovement = animator.GetFloat(VelocityZHash);
+        sidewardMovement = animator.GetFloat(VelocityXHash);
+        // walk direction in normal cordinate system
+        Vector3 direction = new Vector3(sidewardMovement, 0f, forwardMovement);
+
+        float targetAngle = cam.eulerAngles.y; //Mathf.Atan2(velocityZ, velocityX) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+        // make sure character moves in direction of target angle
+        Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * direction; //Vector3.forward;
+        controller.Move(moveDir * speedMultiplier * Time.deltaTime);
+
+        if (controller.isGrounded)
+        {
+            vSpeed = 0; // grounded character has vSpeed = 0...
+            if (Input.GetKeyDown("space"))
+            { // unless it jumps:
+                vSpeed = jumpSpeed;
+            }
+        } else
+        {
+             vSpeed -= gravity * Time.deltaTime;
+        }
+        // apply gravity acceleration to vertical speed:
+       
+        Vector3 vertMove = new Vector3(0f, vSpeed, 0f);
+        controller.Move(vertMove * Time.deltaTime);
+    
+    }
+}
