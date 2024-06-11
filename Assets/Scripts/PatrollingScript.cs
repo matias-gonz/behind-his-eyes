6bd769936ddad387872bf6adb825032ca51a4c1b
matias-gonz <maigonzalez@fi.uba.ar>
2024-06-11 14:30:25 +0200

    Add PathPointsReader

--- Assets/Scripts/PatrollingScript.cs
+++ Assets/Scripts/PatrollingScript.cs
@@ -1,50 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using utils;
 
 public class PatrollingScript : MonoBehaviour
 {
-    private struct PathPoint
-    {
-        public Vector3 Position;
-        public float WaitTime;
-    }
-
     public EnemyControllerScript enemyControllerScript;
     public bool isPatrolling = true;
 
-    private List<PathPoint> PathPoints = new List<PathPoint>();
+    private List<PathPoint> _pathPoints = new List<PathPoint>();
     private int _currentPointIndex = 0;
     private bool _isWaiting = false;
 
 
     void Start()
     {
-        // TODO: Read path points from a file
-        PathPoints.Add(new PathPoint { Position = new Vector3(20, 0, 30), WaitTime = 1 });
-        PathPoints.Add(new PathPoint { Position = new Vector3(20, 0, 40), WaitTime = 1 });
-        PathPoints.Add(new PathPoint { Position = new Vector3(30, 0, 40), WaitTime = 1 });
-        PathPoints.Add(new PathPoint { Position = new Vector3(30, 0, 30), WaitTime = 1 });
+        _pathPoints = PathPointsReader.ReadPathPoints("path1");
     }
 
     void Update()
     {
-        if (!isPatrolling || PathPoints.Count == 0 || _isWaiting)
+        if (!isPatrolling || _pathPoints.Count == 0 || _isWaiting)
         {
             return;
         }
 
         Vector3 position = transform.position;
         position.y = 0;
-        if (Vector3.Distance(position, PathPoints[_currentPointIndex].Position) < 0.1f)
+        if (Vector3.Distance(position, _pathPoints[_currentPointIndex].Position) < 0.1f)
         {
             enemyControllerScript.Stop();
-            _currentPointIndex = (_currentPointIndex + 1) % PathPoints.Count;
-            StartCoroutine(Wait(PathPoints[_currentPointIndex].WaitTime));
+            _currentPointIndex = (_currentPointIndex + 1) % _pathPoints.Count;
+            StartCoroutine(Wait(_pathPoints[_currentPointIndex].WaitTime));
         }
         else
         {
-            enemyControllerScript.MoveTo(PathPoints[_currentPointIndex].Position);
+            enemyControllerScript.MoveTo(_pathPoints[_currentPointIndex].Position);
         }
     }
 
