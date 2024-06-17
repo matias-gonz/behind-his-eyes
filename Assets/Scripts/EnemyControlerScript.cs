19c3a9f9f3221072567deaef9606d5a015f457e3
Jens Hartmann <hartmannje@cip.ifi.lmu.de>
2024-06-17 14:09:31 +0200

    updated enemy script

--- Assets/Scripts/EnemyControlerScript.cs
+++ Assets/Scripts/EnemyControlerScript.cs
@@ -19,6 +19,7 @@ public class EnemyControllerScript : MonoBehaviour
     private readonly float _turnSmoothTime = 0.3f;
     private Vector3 _direction;
     private Vector3 _currentTargetPosition;
+    private bool _isIdle;
     int _velocityXHash; //sidewards animation, can be use for strafe walking
     int _velocityZHash; //forwards animation
 
@@ -31,6 +32,7 @@ public class EnemyControllerScript : MonoBehaviour
         _velocityXHash = Animator.StringToHash("Velocity X");
         _velocityZHash = Animator.StringToHash("Velocity Z");
         _direction = Vector3.zero;
+        _isIdle = false;
     }
 
     private void FixedUpdate()
@@ -41,15 +43,42 @@ public class EnemyControllerScript : MonoBehaviour
             Debug.Log("_direction is zero!!!");
             return;
         }
-
+        
         float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
         float angle = Mathf.SmoothDampAngle(this.transform.eulerAngles.y, targetAngle, ref _angularVelocity,
             _turnSmoothTime);
         Vector3 currentPosition = transform.position;
-        //adjust speed when starting to move or getting close to the goal position
-        float distanceFromGoal = Vector3.Distance(currentPosition, _currentTargetPosition);
-        if (distanceFromGoal < _currentSpeed)
+        
+        float distanceFromGoal = Vector3.Distance(currentPosition, _currentTargetPosition);   
+        calculateCurrentSpeed(distanceFromGoal);
+        Vector3 intermediatePosition = currentPosition + _direction * (_currentSpeed * Time.fixedDeltaTime);
+
+        //move and transform Enemy
+        _rigidbody.MovePosition(intermediatePosition);
+        transform.rotation = Quaternion.Euler(0, angle, 0);
+        // maps  to animation max speed boundaries
+        if (_animator)
         {
+            _animationSpeed = (_currentSpeed / _runningSpeed) * animationMaxSpeed;
+            _animator.SetFloat(_velocityZHash, _animationSpeed);
+        }
+    }
+    //adjust speed when starting to move or getting close to the goal position
+    void calculateCurrentSpeed(float distanceFromGoal)
+    {
+        if (_isIdle)
+        {
+            _currentSpeed = 0f;
+        } else if (distanceFromGoal < 0.1f) 
+        {
+            _currentSpeed = 0f;
+        } else if (_currentSpeed > walkingSpeed && distanceFromGoal < _currentSpeed)
+        {
+            // decelerate from running
+            _currentSpeed -= 4f * Time.fixedDeltaTime;
+        } else if (distanceFromGoal < _currentSpeed/2)
+        {
+            //slowly decelerate from walking
             _currentSpeed -= acceleration * Time.fixedDeltaTime;
         }
         else if (_currentSpeed < _currentMaxSpeed)
@@ -65,28 +94,19 @@ public class EnemyControllerScript : MonoBehaviour
         {
             _currentSpeed = 0f;
         }
-
-        Vector3 intermediatePosition = currentPosition + _direction * (_currentSpeed * Time.fixedDeltaTime);
-        _rigidbody.MovePosition(intermediatePosition);
-        transform.rotation = Quaternion.Euler(0, angle, 0);
-        // maps  to animation max speed boundaries
-        if (_animator)
-        {
-            _animationSpeed = (_currentSpeed / _runningSpeed) * animationMaxSpeed;
-            _animator.SetFloat(_velocityZHash, _animationSpeed);
-        }
     }
-
     public void MoveTo(Vector3 position, bool isRunning)
     {
         //Debug.Log("Guard: New Target to MoveTo");
         _currentTargetPosition = position;
         _currentMaxSpeed = isRunning ? _runningSpeed : walkingSpeed;
+        _isIdle = false;
     }
 
     public void Stop()
     {
-        _currentTargetPosition = transform.position;
+        // _currentTargetPosition = transform.position;
+        _isIdle = true;
     }
 
     private void CalculateDirection()
