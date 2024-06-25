cad6756107ad96090e42490ea03dde2f1084d721
Jens Hartmann <hartmannje@cip.ifi.lmu.de>
2024-06-25 18:13:02 +0200

    fixed scripted gravity. Refactored code for player movement

--- Assets/!/Scripts/ThirdPersonMovement.cs
+++ Assets/!/Scripts/ThirdPersonMovement.cs
@@ -11,24 +11,11 @@ public class ThirdPersonMovement : MonoBehaviour
 
     // constants
     public float speedMultiplier = 3f;
-    public float jumpSpeed = 4f;
     public float gravity = 9.81f;
 
-    // local variable
-    // current vertical velocity, used for falling but not jumping
-    private float _vSpeed = 0f;
-
-    //jumping collider function variables, this corresponds to jumping time/2
-    private float _offsetX = 0.35f;
-
-    // jumping hight
-    private float _offsetY;
-    private float _colliderCenterY;
+    // local variables
     private float _forwardMovement;
     private float _sidewardMovement;
-
-    // private Vector3 currentPosition;
-    private float _jumpStartTime;
     private bool _isGrounded = true;
 
     //hashes
@@ -39,9 +26,6 @@ public class ThirdPersonMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // jumping function y value
-        _offsetY = gravity * Mathf.Pow((_offsetX), 2f);
-        _colliderCenterY = 0.9f;
         _animator = GetComponent<Animator>();
         _rigidbody = GetComponent<Rigidbody>();
         _velocityXHash = Animator.StringToHash("Velocity X");
@@ -70,56 +54,32 @@ public class ThirdPersonMovement : MonoBehaviour
         _rigidbody.Move(intermediatePosition, Quaternion.Euler(0f, targetAngle, 0f));
         // _rigidbody.velocity =
 
-        bool isJump = _animator.GetBool(_isJumpHash);
-        if (!isJump)
+        if (!_animator.GetBool(_isJumpHash))
         {
-            
-            RaycastHit hit = new RaycastHit();
-            if (Physics.Raycast(transform.position, -transform.up, out hit, 10))
-            {
-                float distanceToGround = hit.distance;
-                _isGrounded = distanceToGround <= 0.91f;
-            }
-
-            if (!_isGrounded)
-            {
-                Vector3 gravityVector = new Vector3(0f, -9.8f, 0f);
-                // _rigidbody.AddForce(gravityVector);
-                 _rigidbody.velocity = gravityVector;
-            } else
-            {
-                Debug.Log("is Grounded");
-                _rigidbody.AddForce(Vector3.zero);
-                _rigidbody.velocity = Vector3.zero;
-            }
+            verticalVelocity();
         }
     }
 
-    //  = controller.isGrounded;
-    // if (_animator.GetBool(_isJumpHash))
-    // {
-    //     // jumpAdjustCollider();
-    //     _vSpeed = 0f;
-    // }
-    // else if (_isGrounded)
-    // {
-    //     _vSpeed = 0f; // grounded character has vSpeed = 0...
-    //     //     _vSpeed = jumpSpeed;
-    // }
-    // else
-    // {
-    //     _vSpeed -= gravity * Time.deltaTime;
-    // }
-    // // apply gravity acceleration to vertical speed:
-    // Vector3 vertMove = new Vector3(0f, _vSpeed, 0f);
-    // controller.Move(vertMove * Time.deltaTime);
+    private void verticalVelocity()
+    {
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 10))
+        {
+            float distanceToGround = hit.distance;
+            _isGrounded = distanceToGround <= 0.02f;
+        }
 
-    // public void StartJump()
-    // {
-    //      _rigidbody.useGravity = false;
-    // }
-    // public void StopJump()
-    // {
-    //     _rigidbody.useGravity = true;
-    // }
+        if (!_isGrounded)
+        {
+            Vector3 gravityVector = new Vector3(0f, -9.8f, 0f);
+            _rigidbody.AddForce(gravityVector);
+            // _rigidbody.velocity = gravityVector;
+        }
+        else
+        {
+            // all forces and velocities are reset if standing on ground.
+            _rigidbody.AddForce(Vector3.zero);
+            _rigidbody.velocity = Vector3.zero;
+        }
+    }
 }
