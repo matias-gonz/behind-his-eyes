239c458004d01aa94ca7fde2ffc8d00c0b965466
bytsbyts <Claudia_YJ@163.com>
2024-06-25 17:08:42 +0200

    adding some sounds

--- Assets/!/Scripts/ThirdPersonMovement.cs
+++ Assets/!/Scripts/ThirdPersonMovement.cs
@@ -13,6 +13,7 @@ public class ThirdPersonMovement : MonoBehaviour
     public float speedMultiplier = 3f;
     public float jumpSpeed = 4f;
     public float gravity = 9.8f;
+    public float moveAudioCD = 0;
 
     // local variable
     private float _vSpeed = 0f; // current vertical velocity
@@ -23,6 +24,9 @@ public class ThirdPersonMovement : MonoBehaviour
     private int _velocityXHash;
     private int _velocityZHash;
 
+
+    private AudioSource audio;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +35,7 @@ public class ThirdPersonMovement : MonoBehaviour
         _velocityZHash = Animator.StringToHash("Velocity Z");
         // turns mouse cursor invisible and locks it in place, allowing indefinite mouse movement
         Cursor.lockState = CursorLockMode.Locked;
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -46,7 +51,33 @@ public class ThirdPersonMovement : MonoBehaviour
 
         // make sure character moves in direction of target angle
         Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * direction; //Vector3.forward;
+        //Debug.Log("moveDir" + moveDir);
         controller.Move(moveDir * speedMultiplier * Time.deltaTime);
+
+        moveAudioCD += Time.deltaTime;
+        if (moveDir.x != 0 && !Input.GetKey(KeyCode.LeftShift))
+        {
+
+            string str = _animator.GetBool("isProne") ? "Climb" : "Walk";
+            Debug.Log(str);
+            if (moveAudioCD >= 0.55f && str == "Walk")
+            {
+                StartCoroutine(IsPlayerAudio(str, 0.01f, true));
+                moveAudioCD = 0;
+            }
+            else if (moveAudioCD >= 1f && str == "Climb")
+            {
+                StartCoroutine(IsPlayerAudio(str, 0.01f, true));
+                moveAudioCD = 0;
+            }
+            audio.loop = false;
+        }
+        if (moveDir.x != 0 && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Debug.Log("Run");
+            StartCoroutine(IsPlayerAudio("Run", 0.01f, false));
+            audio.loop = true;
+        }
 
         if (controller.isGrounded)
         {
@@ -55,6 +86,9 @@ public class ThirdPersonMovement : MonoBehaviour
             {
                 // unless it jumps:
                 _vSpeed = jumpSpeed;
+                StartCoroutine(IsPlayerAudio("Jump", 0.5f, true));
+                audio.loop = false;
+                Debug.Log("Jump");
             }
         }
         else
@@ -64,6 +98,35 @@ public class ThirdPersonMovement : MonoBehaviour
         // apply gravity acceleration to vertical speed:
 
         Vector3 vertMove = new Vector3(0f, _vSpeed, 0f);
+        //Debug.Log("vertMove" + vertMove);
         controller.Move(vertMove * Time.deltaTime);
     }
+
+
+
+    IEnumerator IsPlayerAudio(string Clip, float time, bool IsOneShot)
+    {
+        yield return new WaitForSeconds(time);
+        if (IsOneShot)
+        {
+            audio.clip = Resources.Load<AudioClip>("Sounds/" + Clip);
+            audio.PlayOneShot(audio.clip);
+        }
+        else
+        {
+            if (audio.isPlaying)
+            {
+                audio.Stop();
+                audio.clip = Resources.Load<AudioClip>("Sounds/" + Clip);
+                audio.Play();
+                audio.volume = 1;
+            }
+            else
+            {
+                audio.clip = Resources.Load<AudioClip>("Sounds/" + Clip);
+                audio.Play();
+            }
+        }
+    }
 }
+
