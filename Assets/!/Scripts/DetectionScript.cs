27baad69784b39e79ec1e4ae1ad4c761f7114b2b
Matías Ignacio González <maigonzalez@fi.uba.ar>
2024-06-30 16:23:17 +0200

    Add extra rays (#28)

--- Assets/!/Scripts/DetectionScript.cs
+++ Assets/!/Scripts/DetectionScript.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DetectionScript : MonoBehaviour
@@ -8,11 +9,14 @@ public class DetectionScript : MonoBehaviour
     public GameObject target;
     public float fov = 120f;
     public float viewDistance = 10f;
+    public float tolerance = 0.2f;
     private LayerMask _layerMask;
+    private Collider[] _colliders;
 
     private void Start()
     {
         _layerMask = LayerMask.GetMask("Level");
+        _colliders = target.GetComponents<Collider>();
     }
 
     void Update()
@@ -35,14 +39,60 @@ public class DetectionScript : MonoBehaviour
         if (angle > fov / 2) return;
 
         // Check if the target is in sight
-        // TODO: Adjust the ray start and direction once enemy prefab is added
         Vector3 rayStart = transform.position;
-        rayStart.y = 1;
-        Debug.DrawRay(rayStart, distance * playerDirection, Color.red);
-        RaycastHit hit;
-        if (Physics.Raycast(rayStart, playerDirection, out hit, distance, _layerMask)) return;
+        rayStart.y += 1.65f;
 
-        Debug.Log("Target in sight");
+        Collider targetCollider = FindTargetCollider();
+        if (!targetCollider) return;
+
+        Vector3[] directions = GenerateDirections(rayStart, targetCollider);
+
+        if (!directions.Any(direction => CheckRay(direction, rayStart, distance))) return;
+
         AudioManager.Instance.PlaySoundFx("alert");
+    }
+
+    private Collider FindTargetCollider()
+    {
+        return _colliders.FirstOrDefault(c => c.enabled);
+    }
+
+    private Vector3[] GenerateDirections(Vector3 rayStart, Collider targetCollider)
+    {
+        Vector3 center = targetCollider.bounds.center;
+        Vector3 directionTop = targetCollider.bounds.max - rayStart;
+        directionTop.y -= tolerance;
+
+        Vector3 directionBottom = targetCollider.bounds.min - rayStart;
+        directionBottom.y += tolerance;
+
+        Vector3 directionLeft = new Vector3(center.x, center.y, center.z - targetCollider.bounds.extents.z) - rayStart;
+        directionLeft.z += tolerance;
+
+        Vector3 directionRight = new Vector3(center.x, center.y, center.z + targetCollider.bounds.extents.z) - rayStart;
+        directionRight.z -= tolerance;
+
+        Vector3 directionFront = new Vector3(center.x + targetCollider.bounds.extents.x, center.y, center.z) - rayStart;
+        directionFront.x -= tolerance;
+
+        Vector3 directionBack = new Vector3(center.x - targetCollider.bounds.extents.x, center.y, center.z) - rayStart;
+        directionBack.x += tolerance;
+
+        return new[]
+        {
+            directionTop, directionBottom, directionLeft, directionRight, directionFront, directionBack
+        };
+    }
+
+    private bool CheckRay(Vector3 direction, Vector3 rayStart, float distance)
+    {
+        if (Physics.Raycast(rayStart, direction, out _, distance, _layerMask))
+        {
+            Debug.DrawRay(rayStart, direction * distance, Color.green);
+            return false;
+        }
+
+        Debug.DrawRay(rayStart, direction * distance, Color.red);
+        return true;
     }
 }
