8cd0e6556e84964747e6c016bee1fda9c7a50348
matias-gonz <maigonzalez@fi.uba.ar>
2024-07-08 01:17:16 +0200

    Additive scene loading

--- Assets/!/Scripts/GameManager.cs
+++ Assets/!/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -23,13 +24,13 @@ public class GameManager : MonoBehaviour
 
     public void RestartGame()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("CentralCity");
+        SceneManager.LoadScene("CentralCity");
     }
 
     public void GameOver()
     {
         if (godMode) return;
 
-        UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
+        SceneManager.LoadScene("GameOver");
     }
 }
