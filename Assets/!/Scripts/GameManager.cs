386d6d9dc9e904f34c61493d590e9c191ce8e232
matias-gonz <maigonzalez@fi.uba.ar>
2024-07-11 21:54:08 +0200

    Add checkpoint

--- Assets/!/Scripts/GameManager.cs
+++ Assets/!/Scripts/GameManager.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Scene = utils.Scene;
 
+
+
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-
     public bool godMode = false;
+    
+    private Checkpoint? _checkpoint = null;
+    private GameObject _player;
 
     private void Awake()
     {
@@ -22,10 +26,21 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    
+    private void Start()
+    {
+        _player = GameObject.FindWithTag("Player");
+    }
 
     public void RestartGame()
     {
         SceneManager.LoadScene("StreetLevel");
+        Debug.Log(_checkpoint);
+        if (_checkpoint != null && _player)
+        {
+            _player.transform.position = _checkpoint.Value.position;
+            _player.transform.rotation = _checkpoint.Value.rotation;
+        }
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -43,5 +58,10 @@ public class GameManager : MonoBehaviour
         {
             RestartGame();
         }
+    }
+
+    public void SetCheckpoint(Checkpoint checkpoint)
+    {
+        _checkpoint = checkpoint;
     }
 }
