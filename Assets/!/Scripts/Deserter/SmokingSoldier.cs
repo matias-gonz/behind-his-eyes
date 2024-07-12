3fd7d78d68a068e052f70ca7a20b7bb3bfcb9239
Jens Hartmann <hartmannje@cip.ifi.lmu.de>
2024-07-12 14:50:20 +0200

    Added reset of execution animation. The soldier immieadadly starts smoking and the deserter immedieadly lies dead on the floor with no sound being played.

--- Assets/!/Scripts/Deserter/SmokingSoldier.cs
+++ Assets/!/Scripts/Deserter/SmokingSoldier.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class SmokingSoldier : MonoBehaviour
 {
     public GameObject lonelyDeserter;
+    private Animator _animator;
     private Deserter _deserterScript;
+    
+    private int _doExecuteHash;
 
     // Start is called before the first frame update
     void Start()
     {
         _deserterScript = lonelyDeserter.GetComponent<Deserter>();
+        _animator = GetComponent<Animator>();
+        _doExecuteHash = Animator.StringToHash("DoExecute");
+        DisableExecute();
     }
 
+    public void DisableExecute()
+    {
+        _animator.SetBool(_doExecuteHash, false);
+        _deserterScript.GettingKilled();
+    }
 
     private void KillDeserter()
     {
