86ac96298033f15f29264445f9a6a56f3c9beaf8
Matías Ignacio González <maigonzalez@fi.uba.ar>
2024-07-17 16:43:23 +0200

    Revert "Add maxSoundRange in detection script (#54)"
    This reverts commit 6912b86171a93938b925e312db7eeea309a06cd8.

--- Assets/!/Scripts/DetectionScript.cs
+++ Assets/!/Scripts/DetectionScript.cs
@@ -10,17 +10,15 @@ public class DetectionScript : MonoBehaviour
     public GameObject target; 
     public float fov = 120f;
     public float viewDistance = 10f;
-    public float maxSoundRange = 10f;
+    public float closeRange = 2f;
     public float tolerance = 0.2f;
     private LayerMask _layerMask;
     private Collider[] _colliders;
-    private ThirdPersonMovement _thirdPersonMovement;
 
     private void Start()
     {
         _layerMask = LayerMask.GetMask("Level");
         _colliders = target.GetComponents<Collider>();
-        _thirdPersonMovement = target.GetComponent<ThirdPersonMovement>();
         enemyControllerScript.InitialiseEnemyControllerScript(target);
     }
 
@@ -35,14 +33,13 @@ public class DetectionScript : MonoBehaviour
         float distance = Vector3.Distance(transform.position, target.transform.position);
         if (distance > viewDistance) return;
 
-        // Check if the target is in the field of view OR enemy can hear the target
+        // Check if the target is in the field of view
         Vector3 playerDirection = target.transform.position - transform.position;
         playerDirection.y = 0;
         Vector3 lookDirection = transform.forward;
         lookDirection.y = 0;
         float angle = Vector3.Angle(playerDirection, lookDirection);
-        float soundRange = maxSoundRange; // TODO: Implement getSoundRange() from player
-        if (distance > soundRange && angle > fov / 2) return;
+        if (distance > closeRange && angle > fov / 2) return;
 
         // Check if the target is in sight
         Vector3 rayStart = transform.position;
