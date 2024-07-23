020ab7cdf166f24aec1a3d3c9b243543ea7fc268
J3ns3its <168995922+J3ns3its@users.noreply.github.com>
2024-07-23 09:22:29 +0200

    Add dialogue smoking soldier (#75)
    * Added audio script and controller to enemy prefab
    
    * Added dialogue for smoking soldier
    
    * added firing sound enemy
    
    * Added firing sound from audio script for shooting animations
    
    * removed enemy audio script. Enemy now uses playeraudio
    
    ---------
    
    Co-authored-by: Jens Hartmann <hartmannje@cip.ifi.lmu.de>

--- Assets/!/Scripts/PlayerAudio.cs
+++ Assets/!/Scripts/PlayerAudio.cs
@@ -51,6 +51,17 @@ public class PlayerAudio : MonoBehaviour
     {
         PlayAudio("k98cycle", 1f);
     }
+
+    public void SmokeN(int number)
+    {
+        if (number > 4 || number < 0)
+        {
+            UnityEngine.Debug.LogWarning("Sound clip id is out of bounds: " + number);
+            return;
+        }
+        PlayAudio("smoke" + number.ToString(), 1f);
+    }
+    
     public void PlayAudio(string clipName, float volume)
     {
         Audio audioToPlay = System.Array.Find(audioPlayer, a => a.id == clipName);
