e0ca30c7aa56787b033a1f4c02f1ce2c8cd27569
bytsbyts <Claudia_YJ@163.com>
2024-07-23 23:29:04 +0200

    change script to add smoke audio

--- Assets/!/Scripts/Player/PlayerAudio.cs
+++ Assets/!/Scripts/Player/PlayerAudio.cs
@@ -15,6 +15,7 @@ public class PlayerAudio : MonoBehaviour
     public Audio[] fallDownAudio;
     public Audio[] fireK98Audio;
     public Audio[] k98CycleAudio;
+    public Audio[] smokeAudio; 
 
     void Start()
     {
@@ -74,6 +75,16 @@ public class PlayerAudio : MonoBehaviour
     public void CycleK98()
     {
         PlayRandomAudio(k98CycleAudio, 1f);
+    }
+
+    public void SmokeN(int number)
+    {
+        if (number >= smokeAudio.Length || number < 0)
+        {
+            UnityEngine.Debug.LogWarning("Sound clip id is out of bounds: " + number);
+            return;
+        }
+        PlayAudio(smokeAudio[number], 1f);
     }
 
     private void PlayRandomAudio(Audio[] audioArray, float volume)
