11391ffd253b049e1dc94587150bf24c155b5358
Jens Hartmann <hartmannje@cip.ifi.lmu.de>
2024-07-27 17:55:58 +0200

    Added timeline. Player input is disabled during opening camera move

---
+++ Assets/!/Scripts/Level/IntroManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroManager : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerController _playerController;
+
+
+}
