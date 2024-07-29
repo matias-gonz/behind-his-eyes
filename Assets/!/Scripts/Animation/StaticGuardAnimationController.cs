2e57ad7eae7fef1378bd09e004eb1a76feb0a071
Jens Hartmann <hartmannje@cip.ifi.lmu.de>
2024-07-30 01:37:36 +0200

    Added different animation and behaviour for static enemies that can be changed by serialized fields

--- Assets/!/Scripts/Animation/StaticGuardAnimationController.cs
+++ Assets/!/Scripts/Animation/StaticGuardAnimationController.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class StaticGuardAnimationController : MonoBehaviour
 {
     private Animator _animator;
     public bool isSmoking = false;
+    public bool alternativeAnimation = false;
     private int _isSmokingHash;
+    private int _alternativeAnimationHash;
+    private int _idleCounterHash;
 
     void Start()
     {
         _animator = GetComponent<Animator>();
         _isSmokingHash = Animator.StringToHash("IsSmoking");
+        _alternativeAnimationHash = Animator.StringToHash("AlternativeAnimation");
+        _idleCounterHash = Animator.StringToHash("IdleCounter");
+
         _animator.SetBool(_isSmokingHash, isSmoking);
+        _animator.SetBool(_alternativeAnimationHash, alternativeAnimation);
+        _animator.SetInteger(_idleCounterHash, Random.Range(1,6));
+        
     }
 }
